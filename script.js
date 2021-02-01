e6555057a63f08a41fc6924231d0da5feb981e1e
Vincent Chauvaux <vincent.chauvaux@gmail.com>
2021-02-01 15:12:04 +0100

    mis a jour ajax fonction onload

---
+++ script.js
@@ -0,0 +1,12 @@
+    //mes selecteur
+        // var monbtn = document.getElementById('monbtn');
+var quote = document.getElementById('quote');
+    //mes variables
+var citation = "";
+    // mes evenements
+window.onload = async() => {
+    await fetch("https://api.kanye.rest/")
+    .then(reponse => reponse.json())
+    .then(data => citation = data.quote);
+    quote.textContent = citation;
+}
