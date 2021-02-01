923ef114efe22ee777ce939e95dd530732248fa1
Vincent Chauvaux <vincent.chauvaux@gmail.com>
2021-02-01 15:22:38 +0100

    exercice fini

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
     //mes selecteur
-        // var monbtn = document.getElementById('monbtn');
-var quote = document.getElementById('quote');
+        var monbtn = document.getElementById('monbtn');
+        var quote = document.getElementById('quote');
     //mes variables
-var citation = "";
+        var citation = "";
     // mes evenements
-window.onload = async() => {
-    await fetch("https://api.kanye.rest/")
-    .then(reponse => reponse.json())
-    .then(data => citation = data.quote);
-    quote.textContent = citation;
-}
+        window.onload = async() => {
+            await fetch("https://api.kanye.rest/")
+            .then(reponse => reponse.json())
+            .then(data => citation = data.quote);
+            quote.textContent = citation;
+        }
+
+        monbtn.addEventListener("click", async() => {
+            await fetch("https://api.kanye.rest/")
+            .then(reponse => reponse.json())
+            .then(data => citation = data.quote);
+            quote.textContent = citation;
+        })
